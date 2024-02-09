bc7a10edec5ac5f22a46da7e1680e6bc8580f097
ChristianAlteri <alteri.christian@gmail.com>
2024-02-09 08:40:18 +0000

    Api to create stores

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Roboto } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToastProvider } from '@/providers/toast-provider'
+
 
 
 const font = Roboto({
@@ -23,6 +25,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={font.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,43 +1,85 @@
-'use client';
+"use client";
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
-
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { useStoreModal } from "../../hooks/use-store-modal";
 import { Modal } from "../ui/modal";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { useState } from "react";
+
+import { TbFaceIdError, TbFaceId } from "react-icons/tb";
+import toast from "react-hot-toast";
+
+// Custom Toast Error
+const toastError = (message: string) => {
+  toast.error(message, {
+    style: {
+      background: "white",
+      color: "black",
+    },
+    icon: <TbFaceIdError size={30} />,
+  });
+};
+// Custom Toast Success
+const toastSuccess = (message: string) => {
+  toast.error(message, {
+    style: {
+      background: "white",
+      color: "green",
+    },
+    icon: <TbFaceId size={30} />,
+  });
+};
 
 const formSchema = z.object({
-    name: z.string().min(3).max(20),
-})
+  name: z.string().min(3).max(20),
+});
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
+  const [loading, setLoading] = useState(false);
+  const storeModal = useStoreModal();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: ""
-        }
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-        // TODO: Create store
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/stores", values);
+      console.log(response.data);
+      toastSuccess("Store created successfully!");
+    } catch (error: any) {
+      console.log("Error server side post to api/stores", error);
+      toastError("Error creating store");
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return ( 
-        <Modal 
-            title="Welcome to Stella"
-            description="You are now logged in as an admin."
-            isOpen={storeModal.isOpen}
-            onClose={storeModal.onClose}
-            >
-                <div>
+  return (
+    <Modal
+      title="Welcome to Stella"
+      description="You are now logged in as an admin."
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
         <div className="space-y-4 py-2 pb-4">
           <div className="space-y-2">
             <Form {...form}>
@@ -49,25 +91,35 @@ export const StoreModal = () => {
                     <FormItem>
                       <FormLabel>Name</FormLabel>
                       <FormControl>
-                        <Input placeholder="Store Name" {...field} />
+                        <Input
+                          disabled={loading}
+                          placeholder="Store Name"
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
                 <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                  <Button variant="outline" onClick={storeModal.onClose}>
+                  <Button
+                    disabled={loading}
+                    variant="outline"
+                    onClick={storeModal.onClose}
+                  >
                     Cancel
                   </Button>
-                  <Button type="submit">Continue</Button>
+                  <Button disabled={loading} type="submit">
+                    Continue
+                  </Button>
                 </div>
               </form>
             </Form>
           </div>
         </div>
       </div>
-        </Modal>
-     );
-}
+    </Modal>
+  );
+};
 
 export default StoreModal;
