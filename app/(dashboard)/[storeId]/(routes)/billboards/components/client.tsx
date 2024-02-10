41c3ce3651902319c98e000236513168301c31f4
ChristianAlteri <alteri.christian@gmail.com>
2024-02-10 13:37:14 +0000

    added billboard list and edit functionallity

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,17 +3,20 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
 
 import { Plus, PlusCircle, PlusSquare } from "lucide-react";
-// import { columns, BillboardColumn } from "./columns";
+import { columns, BillboardColumn } from "./columns";
 import { useParams, useRouter } from "next/navigation";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BillboardClientProps {
-    // data: BillboardColumn[];
+    data: BillboardColumn[];
+    // data: Billboard[];
   }
   
   export const BillboardClient: React.FC<BillboardClientProps> = ({
-    // data
+    data
   }) => {
     const params = useParams();
     const router = useRouter();
@@ -21,7 +24,7 @@ interface BillboardClientProps {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards"
+          title={`Billboards (${data.length})`}
           description="Manage your featured products"
         />
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)} size="sm">
@@ -30,6 +33,7 @@ interface BillboardClientProps {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
     </>
   );
 };
