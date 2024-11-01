acccb564c51a38944267189f7782f2f1cd568787
ChristianAlteri <alteri.christian@gmail.com>
2024-11-01 15:43:20 +0800

    20241101: logging in webhook

--- app/api/[storeId]/webhook/route.ts
+++ app/api/[storeId]/webhook/route.ts
@@ -17,6 +17,7 @@ function convertUrl(urlFrom: string) {
     
     // Assuming the UUID is the first part of the path after the root '/'
     const uuid = parts[1];
+    console.log("Webhook URL Built",`${baseUrl}/api/${uuid}/verify-webhook-payment`);
   
     // Return the new API route
     return `${baseUrl}/api/${uuid}/verify-webhook-payment`;
@@ -40,11 +41,13 @@ export async function OPTIONS(request: Request) {
 
 export async function POST(req: NextRequest) {
   const sig = req.headers.get("stripe-signature");
+  console.log("sig", sig);
 
   let event;
 
   try {
     const body = await req.text(); // Extract the raw body for Stripe signature verification
+    console.log("body", body);
     if (!sig) {
       console.error("Stripe signature is missing");
       return NextResponse.json(
@@ -53,6 +56,7 @@ export async function POST(req: NextRequest) {
       );
     }
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret!);
+    console.log("event", event);
   } catch (err) {
     console.error(`Webhook Error: ${(err as Error).message}`);
     const errorMessage = (err as Error).message;
@@ -107,6 +111,7 @@ export async function POST(req: NextRequest) {
               });
 
               const data = await response.json();
+              console.log("response data", data);
 
               if (response.ok) {
                 console.log("Metadata successfully passed to API", data);
@@ -155,99 +160,3 @@ export async function POST(req: NextRequest) {
   // Return 200 to acknowledge receipt of the event
   return NextResponse.json({ received: true });
 }
-
-// import { stripe } from "@/lib/stripe";
-// import { NextRequest, NextResponse } from "next/server";
-
-// const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-
-// export async function OPTIONS(request: Request) {
-//   const headers = new Headers();
-//   headers.set("Access-Control-Allow-Origin", "*");
-//   headers.set(
-//     "Access-Control-Allow-Methods",
-//     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-//   );
-//   headers.set(
-//     "Access-Control-Allow-Headers",
-//     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-//   );
-//   headers.set("Access-Control-Allow-Credentials", "true");
-
-//   return new NextResponse(null, { status: 204, headers });
-// }
-
-// export async function POST(req: NextRequest) {
-//   const sig = req.headers.get("stripe-signature");
-
-//   let event;
-
-//   try {
-//     const body = await req.text(); // Extract the raw body for Stripe signature verification
-//     if (!sig) {
-//       console.error("Stripe signature is missing");
-//       return NextResponse.json(
-//         { error: "Stripe signature is missing" },
-//         { status: 400 }
-//       );
-//     }
-//     event = stripe.webhooks.constructEvent(body, sig, endpointSecret!);
-//   } catch (err) {
-//     console.error(`Webhook Error: ${(err as Error).message}`);
-//     const errorMessage = (err as Error).message;
-//     return NextResponse.json(
-//       { error: `Webhook Error: ${errorMessage}` },
-//       { status: 400 }
-//     );
-//   }
-
-//   // Handle different Stripe event types
-//   switch (event.type) {
-//     case "payment_intent.succeeded": {
-//       const paymentIntentSucceeded = event.data.object;
-//       //   console.log("PaymentIntent was successful!", paymentIntentSucceeded);
-//       // Handle logic for successful payment intent
-//       break;
-//     }
-//     case "payment_intent.created": {
-//       const paymentIntentCreated = event.data.object;
-//       console.log("PaymentIntent was created!", paymentIntentCreated);
-//       // Handle logic for payment intent creation
-//       break;
-//     }
-//     case "charge.succeeded": {
-//       const chargeSucceeded = event.data.object;
-//       console.log("Charge was successful!", chargeSucceeded);
-
-//       const paymentIntentId = chargeSucceeded.payment_intent;
-
-//       if (typeof paymentIntentId === 'string') {
-//         // Fetch the PaymentIntent object from Stripe
-//         const paymentIntent = await stripe.paymentIntents.retrieve(
-//           paymentIntentId
-//         );
-
-//         if (paymentIntent.metadata) {
-//           // Attach metadata to your processing logic
-//           console.log("Metadata from PaymentIntent:", paymentIntent.metadata);
-
-//           // Now you can send an API call or attach the metadata as needed
-//         }
-//       }
-
-//       // Handle further logic for successful charge
-//       break;
-//     }
-//     case "terminal.reader.action_succeeded": {
-//       const terminalReaderActionSucceeded = event.data.object;
-//       //   console.log("Terminal reader action was successful!", terminalReaderActionSucceeded);
-//       // Handle logic for terminal reader action succeeded
-//       break;
-//     }
-//     default:
-//       console.log(`Unhandled event type: ${event.type}`);
-//   }
-
-//   // Return 200 to acknowledge receipt of the event
-//   return NextResponse.json({ received: true });
-// }
