b5cec9abfb4ce4e81a83745f9d5de48348629fc0
ChristianAlteri <alteri.christian@gmail.com>
2024-11-29 19:39:13 +0000

    20241129: getting store by if from front end

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,4 +1,5 @@
 import prismadb from "@/lib/prismadb";
+import { convertDecimalFields } from "@/lib/utils";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -149,6 +150,33 @@ export async function DELETE(
     return NextResponse.json(store);
   } catch (error) {
     console.log(`[${logKey}_DELETE]`, error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const storeId = url.searchParams.get('storeId');
+
+    if (!storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+      },
+      include: {
+        address: true,
+      },
+    });
+    // Convert Decimal fields to numbers
+    const storeWithConvertedDecimals = convertDecimalFields(store);
+
+    return NextResponse.json(storeWithConvertedDecimals);
+  } catch (error) {
+    console.log('[API_STORE_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
