578bd8644294a37e801ed978e3e6fb91a2d3b105
ChristianAlteri <alteri.christian@gmail.com>
2024-12-20 14:06:18 +0000

    20241220: No shipping rate for online store test

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -145,7 +145,7 @@ export async function POST(
       shipping_address_collection: { allowed_countries: ["AU"] },
       shipping_options: [
         // { shipping_rate: "shr_1QSammKCnSe3p09QJslPWBG3" }, // Dev
-        { shipping_rate: "shr_1QUPUpKCnSe3p09QMK1xShGT" }, // Prod
+        // { shipping_rate: "shr_1QUPUpKCnSe3p09QMK1xShGT" }, // Prod
       ],
       // allow_promotion_codes: true,
       success_url: `${process.env.FRONTEND_STORE_URL}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
